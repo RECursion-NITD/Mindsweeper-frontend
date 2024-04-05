73e208aac2548fc295edaaa77b3ac2124145b721
Shibshankar0909 <123229119+Shibshankar0909@users.noreply.github.com>
2024-04-06 02:40:29 +0530

    added alerts and changed ui

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import useAuth from '../../hooks/useAuth'
 import { Navigate, useLocation } from 'react-router-dom'
 import "./login.css"
+import { ToastContainer, toast } from 'react-toastify';
 
 const Login = () => {
   const [username, setUsername] = useState("")
@@ -11,17 +12,24 @@ const Login = () => {
 
   const { loginUser, token } = useAuth();
 
-  const submitHandler = async ()=>{
-    const formData = {
-      username: username.trim(),
-      password: password.trim(),
-    };
-    await loginUser(formData)
-  }
+  const submitHandler = async () => {
+    try {
+        const formData = {
+            username: username.trim(),
+            password: password.trim(),
+        };
+        await loginUser(formData);
+    } catch (error) {
+        console.error('Error occurred during login:', error);
+        toast.error(error.response.data.error);
+    }
+};
+
 
   return (
     <>
       {token && <Navigate to={from} />}
+      <ToastContainer/>
       <div className='login-container'>
           <div className='login-heading'>
             Login
