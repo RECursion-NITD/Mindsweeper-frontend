928583834a74f13f5870c33df90d5a686f155c4e
sivam <sivamdas7@gmail.com>
2024-04-06 22:35:45 +0530

    changed input type in game2

--- src/components/game1/Game1.jsx
+++ src/components/game1/Game1.jsx
@@ -196,7 +196,7 @@ const Grid = () => {
         <div key={rowIndex} className="grid-row">
           {row.map((cell, colIndex) => (
             <input
-              inputMode='numeric' 
+              type='number'
               key={colIndex}
               className="grid-cell"
               maxLength="1"
